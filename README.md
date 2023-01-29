559da3c41867814959a8c7f5630a4e3a6b0557de
Taylor Powell <taylorcpowell@gmail.com>
2023-01-28 19:21:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# OOTFest
+
+TODO - Add details
+
+Prerequisites:
+The following file exists ~/ZELOOTD.z64
+
+Run the ootfest-update-linux.sh script:
+./ootfest-update-linux.sh
---
+++ ootfest-update-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Press any key to force an update."
+
+rm -f oot.otr
+
+git fetch --all
+git reset --hard origin/master
+
+if [ ! -f ZELOOTD.z64 ] && [ -f ~/ZELOOTD.z64 ]; then
+    cp ~/ZELOOTD.z64 ZELOOTD.z64
+fi
