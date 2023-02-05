2dee009c65b192096aa5c71efe95c1065b194a09
Taylor Powell <taylorcpowell@gmail.com>
2023-02-05 18:20:14 -0500

    remove hard-coded paths in desktop shortcut

--- ootfest-update-linux.sh
+++ ootfest-update-linux.sh
@@ -11,4 +11,17 @@ if [ ! -f ZELOOTD.z64 ] && [ -f ~/ZELOOTD.z64 ]; then
     cp ~/ZELOOTD.z64 ZELOOTD.z64
 fi
 
+userdir=`pwd`
+
+echo "[Desktop Entry]" > OOTFest.desktop
+echo "Name=OOTFest" >> OOTFest.desktop
+echo "Exec=sh $userdir/soh.sh" >> OOTFest.desktop
+echo "Path=$userdir/" >> OOTFest.desktop
+echo "Icon=$userdir/sohIcon.png" >> OOTFest.desktop
+echo "comment=Ocarina of Time" >> OOTFest.desktop
+echo "Type=Application" >> OOTFest.desktop
+echo "Terminal=false" >> OOTFest.desktop
+echo "Encoding=UTF-8" >> OOTFest.desktop
+echo "Categories=Game;" >> OOTFest.desktop
+
 sudo cp OOTFest.desktop /usr/share/applications/OOTFest.desktop
